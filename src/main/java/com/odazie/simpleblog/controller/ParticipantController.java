8ad6f7f01392b9b99a2e370292160318f3856fc6
Jakub Wróbel <jwrobel@psi.pl>
2023-09-04 09:46:56 +0200

    user correction + localhost:4200

--- src/main/java/com/odazie/simpleblog/controller/ParticipantController.java
+++ src/main/java/com/odazie/simpleblog/controller/ParticipantController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin( origins = "http://localhost:8081" )
+@CrossOrigin( origins = "http://localhost:4200" )
 @RestController
 @RequestMapping("/api")
 public class ParticipantController
